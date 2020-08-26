af44d716bbdd6cd1fe40686263f4412b4de2ffd6
root <carbonkid1985@gmail.com>
2020-08-26 22:37:11 +0100

    updated file structure

---
+++ pxe-release-ubuntu.sh
@@ -0,0 +1,423 @@
+#! /bin/bash
+
+source functions.sh
+
+## global vars ##
+
+nfs_server_ip="192.168.0.2"
+tftp_dir="/data/tftpboot"
+mount_point="/mnt"
+ubuntu_dir="${tftp_dir}/ubuntu"
+desktop_dir="${ubuntu_dir}/desktop"
+server_dir="${ubuntu_dir}/server"
+menu_path="${ubuntu_dir}/ubuntu.menu"
+type_menu_path="${desktop_dir}/desktop.menu"
+default_menu="${tftp_dir}/pxelinux.cfg/default"
+
+## functions
+
+ensure_root ()
+{
+	check_root
+
+	if [[ $? != "0" ]]; then  #Checks for root
+		output "You need to have root privilages to run this script!" red
+		exit 0
+	fi
+}
+
+check_arg ()
+{
+	if [[ ! -z $1 ]]; then
+		arg=$1
+	
+		if [[ -f ${arg} ]]; then
+			file=${arg}
+			ext=${file##*.}
+			if [[ ${ext} == "iso" ]]; then
+				output "Local file, ${file} found." blue
+			else
+				output "File specified does not appear to be an iso disk image." red
+				confirm "Would you like to proceed."  #yes no question
+
+				if [[ $? == "1" ]]; then  #if no
+   				exit 0
+				fi	
+			fi
+			url_file="1"
+		else
+			url=${arg}
+			output "No local file found, URL assumed, ${url}." blue
+			file=/tmp/${url##*/}
+			url_file="0"
+		fi
+		iso=${file##*/}
+	else
+		output "ERROR! Usage: pxe-release-ubuntu.sh [local iso file or URL]..." red
+		exit 0
+	fi
+}
+
+select_flavour ()
+{
+	title="Select Ubuntu flavour"
+	prompt="Pick an option:"
+	options=("Ubuntu"
+	 	"Xubuntu"
+	 	"Lubuntu"
+	 	"Kubuntu"
+	 	"Ubuntu Mate"
+	 	"Ubuntu Budgie"
+ 	 	"Other")
+		
+	output "${title}" green
+	PS3="${prompt} "
+	select opt in "${options[@]}" "Quit"; do
+		case "${REPLY}" in
+			1) flavour="ubuntu"; de="gnome"; menu_flavour="${flavour^}"; menu_de="${de^}";;
+			2) flavour="xubuntu"; de="xfce"; menu_flavour="${flavour^}"; menu_de="${de^^}";;
+			3) flavour="lubuntu"; de="lxde"; menu_flavour="${flavour^}"; menu_de="${de^^}";;
+			4) flavour="kubuntu"; de="kde"; menu_flavour="${flavour^}"; menu_de="${de^^}";;
+			5) flavour="ubuntu_mate"; de="mate"; menu_flavour="Ubuntu Mate"; menu_de="${de^^}";;
+			6) flavour="ubuntu_budgie"; de="budgie"; menu_flavour="Ubuntu Budgie"; menu_de="${de^}";;
+			7) flavour="other";;
+			$(( ${#options[@]}+1 )) ) output "Goodbye!" green; exit 0;;
+			*) output "Invalid option. Try another one." red;continue;;
+	
+		esac
+			
+		if [[ ${flavour} == "other"  ]]; then
+			prompt="Enter ubuntu flavor: "
+			read -p "${prompt}" flavour
+				
+			while [[ -z ${flavour}  ]]; do
+				output "No input entered" red
+				read -p "${prompt}" flavour
+			done
+
+			prompt="Enter desktop environment: "
+			read -p "${prompt}" de
+
+			while [[ -z ${de} ]]; do
+				output "No input entered" red
+				read -p "${prompt}" de
+			done
+			menu_flavour="${flavour^}"
+			menu_de="${de^}"
+		fi
+		output "You entered: ${flavour^}" blue
+		break
+	done
+}
+
+select_version ()
+{
+	prompt="Enter version number: "
+	read -p "${prompt}" version
+	
+	while [ -z ${version} ]; do
+		output "No input entered" red
+		read -p "${prompt}" version
+	done
+	
+	output "You entered: ${version}" blue
+}
+
+dl_file ()
+{
+	if [[ ! -z ${url} ]]; then  ## if the image is a URL
+		confirm "Would you like to download the iso?"  #yes no question
+
+		if [[ $? == "0" ]]; then  #if yes
+			dl_flag="0"
+		else
+			dl_flag="1"
+   		fi
+	fi
+}
+
+fs_create ()
+{
+	confirm "would you like the filesystem created?"  #yes no question
+
+	if [[ $? == "0" ]]; then #if yes
+		fs_flag="0"
+	else
+		fs_flag="1"
+	fi
+}
+
+pxe_menu ()
+{
+	confirm "Would you like a PXE menu entry added?"  #yes no question
+
+	if [[ $? == "0" ]]; then  #if yes
+		pxemenu_flag="0"
+	else
+		pxemenu_flag="1"
+	fi
+}
+
+exports_add ()
+{
+	confirm "Would you like to add an entry to the exports file?"  #yes no question
+
+	if [[ $? == "0" ]]; then  #if yes
+		exports_flag="0"
+	else
+		exports_flag="1"
+	fi
+}
+
+rm_iso ()
+{
+	confirm "Would you like to delete the local iso?"  #yes no question
+
+	if [[ $? == "0" ]]; then  #if yes
+		delete_flag="0"
+	else
+		delete_flag="1"
+	fi
+}
+
+conf_details ()
+{
+	output "See details entered below:" blue
+	if [[ ${url_file} == "0" ]]; then
+		output "URL = ${url}" green
+	else
+		output "FILE = ${file}" green
+	fi
+	#output "URL = ${url}" green
+	#output "FILE = ${file}" green #debug
+	output "ISO = ${iso}" green
+	output "FLAVOUR = ${flavour}" green
+	#output "MENU FLAVOUR = ${menu_flavour}" green #debug 
+	output "DESKTOP = ${de}" green
+	#output "MENU DESKTOP = ${menu_de}" green #debug
+	output "VERSION = ${version}" green
+	
+	if [[ ! -z ${url} ]]; then  ## if the image is a URL
+		if [[ ${dl_flag} == "0" ]]; then
+			msg="true"
+		else
+			msg="false"
+		fi
+		output "DOWNLOAD ISO = ${msg}" green
+	fi
+
+	if [[ ${fs_flag} == "0" ]]; then
+		msg="true"
+	else
+		msg="false"
+	fi
+	output "CREATE & POPULATE FILE STRUCTURE = ${msg}" green
+
+	if [[ ${pxemenu_flag} == "0" ]]; then
+		msg="true"
+	else
+		msg="false"
+	fi
+	output "ADD ENTRY TO PXE MENU = ${msg}" green
+
+	if [[ ${exports_flag} == "0" ]]; then
+		msg="true"
+	else
+		msg="false"
+	fi
+	output "ADD ENTRY TO NFS EXPORTS FILE = ${msg}" green
+
+	if [[ ${delete_flag} == "0" ]]; then
+		msg="true"
+	else
+		msg="false"
+	fi
+	output "DELETE ISO FILE UPON COMPLETION = ${msg}" green
+
+	confirm	"Would you like to proceed? press 'Y' to initiate the unattended release of the PXE image, press 'N' to edit any details:"  #yes no question
+	if [[ $? != "0" ]]; then  #if anything but yes is returned
+		#./$0 ${arg}
+		#/$0 ${arg}
+		$0 ${arg}
+		exit 0
+	fi
+}
+
+create_dir ()
+{
+	output "Creating path ${desktop_dir}/${version}/x64/${de}" blue
+	sudo mkdir -p "${desktop_dir}/${version}/x64/${de}"
+}
+
+mount_iso ()
+{
+	output "Mounting the ISO" blue
+	sudo mount -o loop,ro ${file} ${mount_point}
+}
+
+copy_files ()
+{
+	output "Copying loop files" blue
+	sudo cp -a ${mount_point}/. "${desktop_dir}/${version}/x64/${de}"
+}
+
+umount_iso ()
+{
+	output "Unmounting the ISO" blue
+	sudo umount ${mount_point}
+}
+
+append_menu ()
+{
+	search "menu begin ubuntu" "${default_menu}"
+	if [[ $? != "0" ]]; then
+		output "Adding default menu entry" blue
+
+cat >> "${default_menu}" << EOF
+
+MENU BEGIN Ubuntu
+	MENU TITLE Ubuntu
+        LABEL Previous
+        MENU LABEL Previous Menu
+        TEXT HELP
+        Return to previous menu
+        ENDTEXT
+        MENU EXIT
+        MENU SEPARATOR
+        MENU INCLUDE ubuntu/ubuntu.menu
+MENU END
+
+EOF
+
+	fi
+	if [[ ! -f "${menu_path}" ]]; then
+		output "Creating disto menu" blue
+
+cat > "${menu_path}" << EOF
+# initrd path is relative to pxe root (/tftpboot)
+# nfsroot ip is pxe server's address
+
+EOF
+
+	fi
+	search "menu include ubuntu/desktop/desktop.menu" "${menu_path}"
+	if [[ $? != "0" ]]; then 
+		output "Adding distro menu entry" blue
+
+cat >> "${menu_path}" << EOF
+
+MENU BEGIN Desktop
+MENU TITLE Desktop
+	LABEL Previous
+	MENU LABEL Previous Menu
+	TEXT HELP
+	Return to previous menu
+	ENDTEXT
+	MENU EXIT
+	MENU SEPARATOR
+	MENU INCLUDE ubuntu/desktop/desktop.menu
+MENU END
+
+EOF
+
+	else
+		output "WARNING! Distro menu entry already exists. Skipping" yellow 
+	fi
+   
+	if [[ ! -f "${type_menu_path}" ]]; then
+		output "Creating flavour menu" blue
+
+cat > "${type_menu_path}" << EOF
+# initrd path is relative to pxe root (/data/tftpboot)
+# nfsroot ip is pxe server's address
+
+EOF
+
+   	fi  
+	
+	search "menu label ${menu_flavour} ${version} x64 ${menu_de}" "${type_menu_path}"
+	if [[ $? != "0" ]]; then
+		output "Adding flavour menu entry" blue
+		printf -v rand "%05d" $((1 + RANDOM % 32767))
+		
+cat >> "${desktop_dir}/desktop.menu" << EOF
+LABEL ${rand}
+	MENU LABEL ${menu_flavour} ${version} x64 ${menu_de}
+	KERNEL /ubuntu/desktop/${version}/x64/${de}/casper/vmlinuz
+	INITRD /ubuntu/desktop/${version}/x64/${de}/casper/initrd
+	APPEND ip=dhcp boot=casper text vga=normal netboot=nfs nfsroot=${nfs_server_ip}:${desktop_dir}/${version}/x64/${de} splash --
+	TEXT HELP
+	Boot ${menu_flavour} ${version} x64 ${menu_de}
+ENDTEXT
+
+EOF
+
+	else
+		output "WARNING! Flavour menu entry already exists. Skipping" yellow 
+	fi
+}
+
+append_exports ()
+{
+	search  "${desktop_dir}/${version}/x64/${de}/" "/etc/exports"
+	if [[ $? != "0" ]]; then
+		output "Adding entry to exports" blue
+		echo "${desktop_dir}/${version}/x64/${de}/		192.168.0.0/24(ro,async,no_subtree_check)" >> /etc/exports
+	else
+		output "WARNING! NFS Exports entry already exists. Skipping" yellow 
+	fi
+	}
+
+### Start of script ###
+
+ensure_root
+check_arg $1
+
+# set parameter
+
+select_flavour
+select_version
+dl_file
+fs_create
+pxe_menu
+exports_add
+rm_iso
+conf_details
+
+if [[ ! -z ${url} ]]; then
+	if [[ ${dl_flag} == "0" ]]; then  #if yes
+		output "Download started" blue
+		wget ${url} -O /tmp/${iso}
+
+		if [[ ! -f ${file} ]]; then
+       		output "Unable to find downloaded file. Please check URL" red; exit 0
+   		fi
+	fi
+fi
+
+if [[ ${fs_flag} == "0" ]]; then  #if yes
+   	create_dir
+   	mount_iso
+   	copy_files
+   	umount_iso
+fi
+
+if [[ ${pxemenu_flag} == "0" ]]; then  #if yes
+   	append_menu
+fi
+
+if [[ ${exports_flag} == "0" ]]; then  #if yes
+   	append_exports
+   	output "Restarting nfs server" blue
+   	sudo systemctl restart nfs-kernel-server.service
+fi
+
+if [[ -f ${file} ]]; then
+	if [[ ${delete_flag} == "0" ]]; then  #if yes
+		output "Deleting local iso file ${file}" blue
+     		sudo rm ${file}
+	fi
+fi
+
+output "Success! All operations completed" green
+exit 0
---
+++ pxe-setup.sh
@@ -0,0 +1,175 @@
+#! /bin/bash
+
+source functions.sh
+
+## global vars
+#nfs_server_ip=192.168.0.2
+tftpd_conf="/etc/default/tftpd-hpa"
+dhcpd_conf="/etc/dhcp/dhcpd.conf"
+tftp_dir="/data/tftpboot/"
+syslinux_dir="/usr/lib/syslinux/modules/bios/"
+pxelinux_dir="/usr/lib/PXELINUX/"
+#mount_point="/mnt"
+
+filestructure_setup ()
+{
+     	if [[ ! -f "${tftp_dir}pxelinux.cfg/pxe.conf" ]]; then
+	       	confirm "Configure pxe menu conf file?"
+		if [[ $? == "0" ]]; then # if yes
+			if [[ ! -f "${tftp_dir}pxelinux.cfg/" ]]; then
+	       			printf "%s\n" "Creating file structure"
+		 		mkdir -p ${tftp_dir}pxelinux.cfg
+			fi
+		 	printf "%s\n" "Creating pxe menu conf"
+cat > "${tftp_dir}pxelinux.cfg/pxe.conf" << EOF
+MENU TITLE  Pavey's PXE Server
+MENU BACKGROUND pxelinux.cfg/pxe_splash.png
+NOESCAPE 1
+ALLOWOPTIONS 1
+PROMPT 0
+menu width 80
+menu rows 14
+MENU TABMSGROW 24
+MENU MARGIN 10
+menu color title		1;36;44		#ff8950fc #00000000 std
+menu color sel			7;37;40		#ff8950fc #00000000 std
+menu color unsel		37;44		#c04f4e63 #00000000 std
+menu color help			37;40		#c0ffffff #00000000 std
+menu color border		51;153;255	#00ffffff #00000000 none
+EOF
+		fi
+		search "vesamenu.c32" "${tftp_dir}pxelinux.cfg/default"
+		if [[ -f "${tftp_dir}pxelinux.cfg/default" ]] || [[ $? != "0" ]]; then
+			printf "%s\n" "Creating default menu"
+cat > "${tftp_dir}pxelinux.cfg/default" << EOF
+DEFAULT vesamenu.c32 
+TIMEOUT 50
+ONTIMEOUT BootLocal
+PROMPT 0
+MENU INCLUDE pxelinux.cfg/pxe.conf
+NOESCAPE 1
+
+LABEL BootLocal
+        localboot 0
+        TEXT HELP
+        Boot from local hard disk
+ENDTEXT
+
+EOF
+		fi
+		confirm "Pull down background image?"
+		if [[ $? == "0" ]]; then # if yes
+			wget https://i.imgur.com/ktEA3WS.png -O /tmp/pxe_splash.png
+			if [[ -f "/tmp/pxe_splash.png" ]]; then
+				printf "%s\n" "File saved in ${tftp_dir}pxelinux.cfg/pxe_splash.png"  
+				mv /tmp/pxe_splash.png ${tftp_dir}pxelinux.cfg/pxe_splash.png
+			else
+				printf "%s\n" "Error downloading image"
+			fi
+		fi 
+	fi
+}
+
+#services_setup ()
+#{
+#}
+
+tftpd_setup ()
+{
+	confirm "Install tftpd-hpa?"
+	if [[ $? == "0" ]]; then # if yes
+	       	printf "%s\n" "Installing TFTPD"
+		apt install -y tftpd-hpa
+		printf "%s\n" "Modifying ${tftpd_conf}"
+cat > "${tftpd_conf}" << EOF
+# /etc/default/tftpd-hpa
+
+TFTP_USERNAME="tftp"
+TFTP_DIRECTORY="/data/tftpboot"
+TFTP_ADDRESS=":69"
+TFTP_OPTIONS="--secure --verbose"
+EOF
+	fi
+}
+
+dhcpd_setup ()
+{
+	confirm "Install isc-dhcpd"
+	if [[ $? == "0" ]]; then # if yes
+		printf "%s\n" "Installing DHCPD"
+		apt install -y isc-dhcp-server
+		printf "%s\n" "Modifying ${dhcpd_conf}"
+cat > "${dhcpd_conf}" << EOF
+# dhcpd.conf
+
+default-lease-time 600;
+max-lease-time 7200;
+
+authoritatve;
+
+# DHCP declaration including PXE boot filename
+
+subnet 192.168.0.0 netmask 255.255.255.0 {
+        range 192.168.0.10 192.168.0.254;
+        default-lease-time 21600;
+        max-lease-time 43200;
+        next-server 192.168.0.2;
+        option routers 192.168.0.1;
+        option domain-name-servers 192.168.0.2;
+        filename "/lpxelinux.0";
+}
+EOF
+	fi
+}
+
+nfs_setup ()
+{
+	confirm "Install nfs-kernel-server"
+	if [[ $? == "0" ]]; then # if yes
+		printf "%s\n" "Installing NFS"
+		apt install -y nfs-kernel-server
+	fi
+}
+syslinux_setup ()
+{
+	confirm "Install syslinux?"
+	if [[ $? == "0" ]]; then # if yes
+		printf "%s\n" "Installing syslinux"
+		apt install -y syslinux
+		printf "%s\n" "Copy required files from ${syslinux_dir}"
+		cp -a ${syslinux_dir}. ${tftp_dir}
+	fi
+}
+
+pxelinux_setup ()
+{
+     	confirm "Install pxelinux?"
+      	if [[ $? == "0" ]]; then # if yes
+     		printf "%s\n" "Installing pxelinux"
+     		apt install -y pxelinux
+     		printf "%s\n" "Copy required files from ${pxelinux_dir}"
+     		cp -a ${pxelinux_dir}lpxelinux.0 ${tftp_dir}
+	fi
+}
+## Start of script
+
+check_root
+
+if [[ $? != "0"  ]]; then # returns 0 if root
+     	printf "%s\n" "You need to be root"
+	exit 0
+fi
+
+confirm "Setup filestructure"
+if [[ $? == "0" ]];then
+	filestructure_setup
+fi
+
+confirm "Install services?"
+if [[ $? == "0" ]]; then # if yes
+  	tftpd_setup
+    	dhcpd_setup
+      	nfs_setup
+	syslinux_setup
+      	pxelinux_setup
+fi
